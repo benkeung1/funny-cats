58ae08146dee4eeec32de946c263406bd9d15a53
Ben Keung <ben.keung2011@gmail.com>
2019-11-02 20:46:15 +0800

    update

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const searchBox = ({searchChange}) => {
       <input 
         className="pa3 ba b--green bg-lightest-blue"
         type="search" 
-        placeholder="search robots" 
+        placeholder="search cats" 
         onChange={searchChange}
         />
     </div>
